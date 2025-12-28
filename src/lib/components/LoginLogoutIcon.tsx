ef0b2a300f6705d2e8e7183382ba66b6deee690e
Laurel Mossor <laurel.mossor@gmail.com>
2025-12-27 18:55:17 -0800

    Refactor nav buttons
    Signed-off-by: Laurel Mossor <laurel.mossor@gmail.com>

---
+++ src/lib/components/LoginLogoutIcon.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
+import { LogoutIcon, LoginIcon } from "./icons";
+
+interface LoginLogoutIconProps {
+	isLoggedIn: boolean;
+}
+
+const iconButtonStyles = "p-2 hover:bg-soft-grey rounded transition-colors";
+
+export function LoginLogoutIcon({ isLoggedIn }: LoginLogoutIconProps) {
+	const router = useRouter();
+
+	const handleLogout = async () => {
+		await signOut({ callbackUrl: "/collections" });
+	};
+
+	const handleLogin = () => {
+		router.push("/login");
+	};
+
+	return isLoggedIn ? (
+		<button
+			onClick={handleLogout}
+			className={iconButtonStyles}
+			aria-label="Log Out"
+			title="Log Out"
+		>
+			<LogoutIcon />
+		</button>
+	) : (
+		<button
+			onClick={handleLogin}
+			className={iconButtonStyles}
+			aria-label="Log In"
+			title="Log In"
+		>
+			<LoginIcon />
+		</button>
+	);
+}
+
