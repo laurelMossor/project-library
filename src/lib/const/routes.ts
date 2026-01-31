16394bc8de68621f16c56d1535d32b8a73449255
Laurel Mossor <laurel.mossor@gmail.com>
2026-01-30 16:43:37 -0800

    add themed surface versions
    Signed-off-by: Laurel Mossor <laurel.mossor@gmail.com>

--- src/lib/const/routes.ts
+++ src/lib/const/routes.ts
@@ -16,7 +16,6 @@ export const USER_PROFILE_SETTINGS = "/u/profile/settings";
 export const USER_PROFILE_EDIT = "/u/profile/edit";
 export const PUBLIC_USER_PAGE = (username: string) => `/u/${username}`;
 export const USER_CONNECTIONS = (username: string) => `/u/${username}/connections`;
-// USER_COLLECTIONS removed - collections are shown inline on public profile
 
 // ============================================================================
 // Org & Profile Routes
@@ -36,8 +35,6 @@ export const PROJECTS = "/projects";
 export const PROJECT_NEW = "/projects/new";
 export const PROJECT_DETAIL = (id: string) => `/projects/${id}`;
 export const PROJECT_EDIT = (id: string) => `/projects/${id}/edit`;
-export const PROJECT_ENTRIES = (id: string) => `/projects/${id}/entries`; // Deprecated - use PROJECT_POSTS
-export const PROJECT_ENTRY_NEW = (id: string) => `/projects/${id}/entries/new`; // Deprecated - use PROJECT_POST_NEW
 export const PROJECT_POSTS = (id: string) => `/projects/${id}/posts`;
 export const PROJECT_POST_NEW = (id: string) => `/projects/${id}/posts/new`;
 
@@ -71,9 +68,6 @@ export const API_ME_OWNER = "/api/me/owner"; // GET current owner, PUT to switch
 export const API_USER_PROJECTS = (username: string) => `/api/users/${username}/projects`;
 export const API_USER_EVENTS = (username: string) => `/api/users/${username}/events`;
 
-// Legacy API Routes (deprecated - use new routes above)
-export const API_PROFILE = "/api/profile"; // Deprecated - use API_ME_USER
-
 // Project API Routes
 export const API_PROJECTS = "/api/projects";
 export const API_PROJECT_UPLOAD = "/api/projects/upload";
@@ -102,5 +96,5 @@ export const DEV_TAXONOMY = "/dev/taxonomy";
 
 export const FEEDBACK_SURVEY = "https://docs.google.com/forms/d/e/1FAIpQLScQeZneNUq6QhpJ_dbIJ2-E7zr186HFer9V5x6kDSb0Bzxl8A/viewform?usp=header"
 export const BUG_REPORT_FORM = "https://docs.google.com/forms/d/e/1FAIpQLScfIyo6yd_EvuJw4xJH-FFBgNid73QIGkAWaxUHVnSpgPbE4Q/viewform?usp=dialog";
-export const GITHUB_REPO = "https://github.com/project-library/project-library";
+export const GITHUB_REPO = "https://github.com/laurelMossor/project-library";
 export const INSTAGRAM = "https://instagram.com/laurelmossor";
